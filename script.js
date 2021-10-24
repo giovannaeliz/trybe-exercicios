49c5e0a67be74118f8299d3587cc1f02e4745cbd
Giovanna <giovannaelizz@hotmail.com>
2021-10-24 15:21:02 -0300

    adicionando arquivos html css e js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("header-container").style.backgroundColor = "pink";
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "lightgreen";
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "lightblue";
+document.getElementsByTagName("h3")[0].style.backgroundColor = "purple"
+document.getElementsByTagName("h3")[1].style.backgroundColor = "purple"
+document.getElementsByTagName("h3")[2].style.backgroundColor = "darksalmon"
+document.getElementsByTagName("h3")[3].style.backgroundColor = "darksalmon"
